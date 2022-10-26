4f44d92ab69f16435d3b7c5005aba1f458bd1d32
Thyjano <116716271+Thyjano@users.noreply.github.com>
2022-10-26 11:30:53 +0200

    Add files via upload

---
+++ Webshop/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html  lang="en" dir="ltr" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="utf-8">
+        <title>Sidebar  Dashboard Template</title>
+        <link rel="stylesheet" href="Stylesheet.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    </head>
+    <body>
+    <!--header area start -->
+    <header>
+        <div class="left_area">
+            <h3>Thy<span>Sneaks</span></h3>
+    </header>
+    <!--header area end-->
+    <div class="container">
+    <img src="img/j1sil.jpg" class="Jordan1sil" alt="">
+    <img src="img/j3sil.jpg" class="Jordan3sil" alt="">
+    <img src="img/j4sil.jpg" class="Jordan4sil" alt="">
+    <img src="img/j11sil.jpg" class="Jordan11sil" alt="">
+        <p>
+        <p1>Jordan 1</p1>
+        <p2>Jordan 2</p2>
+        <p3>Jordan 4</p3>
+        <p4>Jordan 11</p4>
+        </p>
+    </div>
+    </body>
+</html>
